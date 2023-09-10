23a25164295d3859488924dcc175a2e846e979b3
kame6821 <kame6821@colorado.edu>
2023-09-10 01:24:55 +0000

    gave my_utils a named argument, updated print_fires accordingly, and made a run.sh file

--- my_utils.py
+++ my_utils.py
@@ -1,9 +1,8 @@
-def get_column(file_name, query_column, query_value, result_column):
+def get_column(file_name, query_column, query_value, result_column=1):
     f = open(file_name, 'r')
     result_column_array = []
     for l in f:
         A = l.rstrip().split(',')
         if A[query_column] == query_value:
-            print(A[query_column])
             result_column_array.append(A[result_column])
     return result_column_array
--- print_fires.py
+++ print_fires.py
@@ -4,5 +4,5 @@ country='United States of America'
 country_column = 0
 forest_fires_column = 3
 file_name = 'Agrofood_co2_emission.csv'
-forest_fires = get_column(file_name, country_column, country, forest_fires_column)
+forest_fires = get_column(file_name, country_column, country, result_column=forest_fires_column)
 print(forest_fires)
