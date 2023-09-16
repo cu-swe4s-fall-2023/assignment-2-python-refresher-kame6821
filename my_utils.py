e6891ad4b4444584ba564f15acbea30994534fdd
kame6821 <kame6821@colorado.edu>
2023-09-16 22:47:37 +0000

    updated my_utils to follow style guide

--- my_utils.py
+++ my_utils.py
@@ -2,9 +2,10 @@
     * get_column -- returns desired information from data file
 """
 
+
 def get_column(file_name, query_column, query_value, result_column=1):
-    """Searches data file for matches in one column and returns data from another.
-    
+    """Searches file for matches in one column and returns data from another
+
     Parameters
     ----------
     file_name : str
@@ -12,10 +13,10 @@ def get_column(file_name, query_column, query_value, result_column=1):
     query_column: int
         Column index to be searched to match query_value
     query_value: str
-        The value to match to finding data on it in other columns 
+        The value to match to finding data on it in other columns
     result_col: int (default = 1)
         Column index with desired info to be returned
-    
+
     Returns
     -------
     result_column_array
@@ -23,11 +24,11 @@ def get_column(file_name, query_column, query_value, result_column=1):
     """
     f = open(file_name, 'r')
     result_column_array = []
-    for l in f:
-        A = l.rstrip().split(',')
+    for line in f:
+        A = line.rstrip().split(',')
         if A[query_column] == query_value:
             result_column_array.append(A[result_column])
-        
+
     try:
         result_column_array = [int(float(i)) for i in result_column_array]
     except ValueError:
