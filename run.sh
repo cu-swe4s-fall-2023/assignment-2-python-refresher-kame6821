a55aa5c3fe869cf80babb57444c42799e7d53e6a
kame6821 <kame6821@colorado.edu>
2023-09-16 23:01:36 +0000

    added examples of functioning runs and two runs that will trigger errors to run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-set -e # stop on error
-set -u # raise error if variable is unset
+set -e # Stop on error
+set -u # Raise error if variable is unset
 
-python print_fires.py
+echo # To make outputs easier to read
+
+# Example of functioning inputs
+python print_fires.py --file_name "Agrofood_co2_emission.csv" --country "United States of America" --country_column 0 --result_column 3
+echo
+
+# Example of input that will raise error in get_column
+python print_fires.py --file_name "Agrofood_co2_emission.csv" --country "United States of America" --country_column 0 --result_column 0
+echo
+
+# Example of input that will raise error in print_fires
+python print_fires.py --file_name "Agrofood_co2_emission.csv" --country "United States of America" --country_column 0 --result_column 31
+echo
