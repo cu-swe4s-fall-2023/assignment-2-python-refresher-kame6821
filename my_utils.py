e747a5b76818ff3e9e0151272e3d6496cd317c41
kame6821 <kame6821@colorado.edu>
2023-09-16 22:36:50 +0000

    updated my_utils to return integers and added documentation

--- my_utils.py
+++ my_utils.py
@@ -1,8 +1,31 @@
+"""Library of math functions:
+    * get_column -- returns desired information from data file
+"""
+
 def get_column(file_name, query_column, query_value, result_column=1):
+    """Searches data file for matches in one column and returns data from another.
+    
+    Parameters
+    ----------
+    file_name : str
+        Name of file (i.e. csv or txt) with delimited rows and columns of data
+    query_column: int
+        Column index to be searched to match query_value
+    query_value: str
+        The value to match to finding data on it in other columns 
+    result_col: int (default = 1)
+        Column index with desired info to be returned
+    
+    Returns
+    -------
+    result_column_array
+        List of data entries from desired subset
+    """
     f = open(file_name, 'r')
     result_column_array = []
     for l in f:
         A = l.rstrip().split(',')
         if A[query_column] == query_value:
             result_column_array.append(A[result_column])
-    return result_column_array
+    int_result_column_array = [int(float(i)) for i in result_column_array]
+    return int_result_column_array
