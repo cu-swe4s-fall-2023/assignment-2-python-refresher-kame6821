eb1d4cf6af80ae668beb7083c29107ea4a11bbb5
kame6821 <kame6821@colorado.edu>
2023-10-17 14:21:48 +0000

    saving for version control -- need to update snakmake to take a text file input

--- src/run.sh
+++ src/run.sh
@@ -5,4 +5,4 @@ set -u # Raise error if variable is unset
 echo # To make outputs easier to read
 
 # Run to get populations comparison
-python data_analysis.py --file_name "../data/Agrofood_co2_emission.csv" --country_list "China" "Russia" "Nigeria" "United States of America" "Brazil" "Australia" --country_column 0 --result_column_list 1 29 --x_y_labels "Year" "Number of People" --plot_name "Total Population Trend"
+python data_analysis.py --file_name "../data/Agrofood_co2_emission.csv" --country_list "China" "Russian Federation" "Nigeria" "United States of America" "Brazil" "Australia" --country_column 0 --result_column_list 1 28 --x_y_labels "Year" "Number of Males" --plot_name "Total Population Trend"
